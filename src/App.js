4812c10d2649b906fa948d06aa78d8aa7a9876d4
Ari <ariannatnz@gmail.com>
2023-07-16 23:26:28 +1200

    Created Landing Page
    The Landing page is now done!! And it has links to either customer login or farmer login.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import './App.css';
+import {
+  Routes, Route,
+} from 'react-router-dom';
+import Landing from './Components/Landing';
+import FarmerLogin from './Components/FarmerLogin';
+import CustomerLogin from './Components/CustomerLogin';
 
 export default function App (){
   return (
-    <div className="overlap">
-    <div className="overlap-group">
-      <h1 className="text-wrapper">Farmer to Foodie</h1>
-      <img
-        className="rectangle"
-        alt="Rectangle"
-        src="https://generation-sessions.s3.amazonaws.com/68eb6af8c3c87c795c724916e6e050ee/img/rectangle-1.png"
-      />
-      <p className="p">Discover the roots of your food: empowering farmers and enriching communities!</p>
-    </div>
-    <div className="p-wrapper">
-      <p className="text-wrapper-2">I want to locally sell my farmed produce</p>
-    </div>
-    <div className="div">
-      <p className="text-wrapper-3">I want to buy fresh, local produce</p>
-    </div>
-  </div>
+    <Routes>
+      <Route path="/FarmerLogin" element={<FarmerLogin/>} />
+      <Route path="/CustomerLogin" element={<CustomerLogin/>} />
+      <Route path="/" element={<Landing />}></Route>
+    </Routes>
   );
 };
---
+++ src/Components/Landing.jsx
@@ -0,0 +1,27 @@
+const Landing = () => {
+    return (
+        <div className="overlap">
+        <div className="overlap-group">
+            <h1 className="text-wrapper">Farmer to Foodie</h1>
+            <img
+            className="rectangle"
+            alt="Rectangle"
+            src="https://generation-sessions.s3.amazonaws.com/68eb6af8c3c87c795c724916e6e050ee/img/rectangle-1.png"
+            />
+            <p className="p">Discover the roots of your food: empowering farmers and enriching communities!</p>
+        </div>
+        <a href="./Components/FarmerLogin">
+            <span className="p-wrapper">
+                <p className="text-wrapper-2">I want to locally sell my farmed produce</p>
+            </span>
+        </a>
+        <a href="./Components/CustomerLogin">
+            <span className="span">
+                <p className="text-wrapper-3">I want to buy fresh, local produce</p>
+            </span>
+        </a>
+        </div>
+    );
+}
+
+export default Landing;
